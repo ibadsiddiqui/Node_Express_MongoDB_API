0aa8f1757f67156b0007bf490e80c150219a2335
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-09-21 16:51:38 +0500

    nodemon dev setting added along with resquest and response added

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,6 +1,8 @@
 module.exports = (app, db) => {
     app.post('/notes', (req,res) => {
         // You'll create your note here
+        console.log("user is notes url")
+        console.log(req.body);
         res.send('Salam')
     })
 }
