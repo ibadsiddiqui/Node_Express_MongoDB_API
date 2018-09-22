7f0e7d6546a87f3c4de4048d218198c8f2cc02e5
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-09-23 01:06:59 +0500

    delete method added for the API

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,3 +1,6 @@
+// MongoDB requires not just an ID as a string, 
+// but as an ID object or, as they call it, an ObjectID
+
 var ObjectID = require('mongodb').ObjectID
 module.exports = (app, db) => {
     app.get('/notes/:id', (req, res) => {
@@ -13,7 +16,7 @@ module.exports = (app, db) => {
                 console.log('successfully found data')
             }
         })
-    })
+    });
     
     
     app.post('/notes', (req, res) => {
@@ -30,6 +33,21 @@ module.exports = (app, db) => {
             } else {
                 res.send(result.ops[0]);
                 console.log('successfully inserted')
+            }
+        })
+    });
+
+
+    app.delete('/notes/:id', (req, res) => {
+        // You'll create your note here
+        const details = {'_id': new ObjectID(req.params.id)};
+
+        db.collection('notes').deleteOne(details, (err, item) => {
+            if (err) {
+                res.send({ 'error': 'An error has occurred' });
+            } else {
+                res.send("Note " + req.params.id + " deleted.");
+
             }
         })
     })
