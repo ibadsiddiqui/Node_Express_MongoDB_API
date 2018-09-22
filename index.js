bbb7f4ec4d197182eb593ce5f487aba120a9375d
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-09-22 12:04:55 +0500

    express added along with path and connection to mongodb locally

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 // server
 
-const express  = require('express');
+const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
-const app =express();
+const db = require('./config/db');
+
+const app = new express();
 const port = 8000;
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+MongoClient.connect(db.url, (err, database) => {
+    if (err)
+        return console.log(err);
     
+    
+    database = database.db("example")
+    require('./app/routes')(app, {});
 
-app.use(bodyParser.urlencoded({extended: true}));
 
-
-require('./app/routes')(app, {});
-
-app.listen(port, () => {
-    console.log('Listening on port: ' + port);
+    app.listen(port, () => {
+        console.log('Listening on port: ' + port);
+    })
 })
