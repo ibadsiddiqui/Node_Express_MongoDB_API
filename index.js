51a9dfe820f780d1b97c6672301763d0721af6ba
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-09-22 11:23:36 +0500

    typo fixed

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const app =express();
 const port = 8000;
-
+    
 
 app.use(bodyParser.urlencoded({extended: true}));
 
